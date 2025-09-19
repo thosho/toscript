438316279e146eb3c21f3608ab0d24d14c1c0892
thosho <thosho.tech@gmail.com>
2025-09-20 02:07:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -294,6 +294,7 @@ FADE OUT.`;
             if (scriptHeader) scriptHeader.style.display = 'flex';
             renderEnhancedScript();
         } else if (view === 'card') {
+            projectData.projectInfo.scenes = extractScenesFromText(fountainInput.value);
             cardView?.classList.add('active');
             if (cardHeader) cardHeader.style.display = 'flex';
             renderEnhancedCardView();
